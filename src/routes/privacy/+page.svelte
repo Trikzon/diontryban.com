cd1b21b0ce8db71fcb316bdf7e2aa0c0a6cadee7
Dion Tryban <github@diontryban.com>
2024-01-01 22:12:03 -0500

    Update privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -2,4 +2,4 @@
     <title>Privacy — Dion Tryban</title>
 </svelte:head>
 
-<iframe width="100%" height="500px" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+I don't collect your data. Stop sending it to me.
