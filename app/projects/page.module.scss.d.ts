c5233089d8cd10bb06d073fc6fa277e217f6b051
Dion Tryban <dion@tryban.dev>
2025-07-22 03:30:57 -0400

    feat(projects): add date sorting and year hr

--- app/projects/page.module.scss.d.ts
+++ app/projects/page.module.scss.d.ts
@@ -6,6 +6,7 @@ export type Styles = {
   'summary': string;
   'tag': string;
   'tags': string;
+  'year-hr': string;
 };
 
 export type ClassNames = keyof Styles;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,50 +1,89 @@
 import styles from "./page.module.scss";
 
+interface Project {
+  title: string;
+  slug: string;
+  summary: string;
+  tags: string[];
+  dateStarted: Date;
+}
+
 const Projects = () => {
-  const projects = [
+  const allProjects: Project[] = [
     {
       title: "tryban.dev",
       slug: "tryban-dev",
-      summary: "This website! Built with Next.js, React, and Content Collections",
+      summary: "This website! Built with Next.js, React, and Content Collections.",
       tags: ["completed", "godot", "school", "mc-mod"],
+      dateStarted: new Date("2025-01-01"),
     },
     {
       title: "Ash API",
       slug: "ash-api",
       summary: "A Minecraft mod library that wraps around the Fabric and Neoforge modloader APIs.",
       tags: ["completed", "godot", "school", "mc-mod"],
+      dateStarted: new Date("2023-01-01"),
     },
   ];
+
+  const sortedProjects = allProjects.sort(
+    (a, b) => b.dateStarted.getTime() - a.dateStarted.getTime()
+  );
+
+  const projectsByYear = sortedProjects.reduce(
+    (acc, project) => {
+      const year = project.dateStarted.getUTCFullYear();
+
+      if (!acc[year]) {
+        acc[year] = [];
+      }
+      acc[year].push(project);
+
+      return acc;
+    },
+    {} as Record<number, Project[]>
+  );
+
+  const sortedYears = Object.keys(projectsByYear).map(Number).sort((a, b) => b - a);
 
   return (
     <>
       <h1>Projects</h1>
-      <div className={styles["project-list"]}>
-        {projects.map((project) => (
-          <div key={project.slug} className={styles.project}>
-            <div className={styles.logo}>
-              <img src={`/projects/${project.slug}/logo.png`} alt={`${project.title} logo`} />
+      {sortedYears.map((year, index) => (
+        <div key={year}>
+          {index != 0 && 
+            <div className={styles["year-hr"]} role="separator" aria-orientation="horizontal" aria-label={`${year}`}>
+              —{year}—
             </div>
-            <div className={styles.info}>
-              <h2>
-                <a href={`/project/${project.slug}`}>
-                  {project.title}
+          }
+          <ul className={styles["project-list"]}>
+            {projectsByYear[year].map((project) => (
+              <li key={project.slug} className={styles.project}>
+                <a href={`/projects/${project.slug}`} className={styles.logo}>
+                  <img src={`/projects/${project.slug}/logo.png`} alt={`${project.title} logo`} />
                 </a>
-              </h2>
-              <div className={styles.summary}>
-                {project.summary}
-              </div>
-              <div className={styles.tags}>
-                {project.tags.map((tag) => (
-                  <div key={`${project.slug}#${tag}`} className={styles.tag}>
-                    #{tag}
+                <div className={styles.info}>
+                  <h2>
+                    <a href={`/project/${project.slug}`}>
+                      {project.title}
+                    </a>
+                  </h2>
+                  <div className={styles.summary}>
+                    {project.summary}
                   </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+                  <div className={styles.tags}>
+                    {project.tags.map((tag) => (
+                      <div key={`${project.slug}#${tag}`} className={styles.tag}>
+                        #{tag}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </>
   );
 };
